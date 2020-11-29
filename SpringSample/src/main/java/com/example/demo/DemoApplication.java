f77d86a1521ecdd386624e08215ff2900081425d
kurasher <27tonokurasher07@gmail.com>
2020-11-29 22:27:55 +0900

    [add] SpringSample

---
+++ SpringSample/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// コマンドプロンプトでの実行方法
+//C:\workspace\git\JavaWork\SpringBoot\SpringSample配下
+// $ gradlew.bat bootRun
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@RequestMapping("/")
+	String index(){
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
