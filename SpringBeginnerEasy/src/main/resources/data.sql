5e7f0bb7d328ab3ed297f3d95c0b71351749956a
kurasher <27tonokurasher07@gmail.com>
2020-12-23 21:03:27 +0900

    [add] seconde commit

---
+++ SpringBeginnerEasy/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO inquiry(name, email, contents, created) VALUES('Ethan', 'sample@sample.com', 'Hello', '2020-12-23 20:02:00');
+INSERT INTO inquiry(name, email, contents, created) VALUES('Emma', 'sample2@sample.com', 'Good Morning', '2020-12-23 20:02:00');
+INSERT INTO inquiry(name, email, contents, created) VALUES('William', 'sample3@sample.com', 'Good Evening', '2020-12-23 20:02:00');
---
+++ SpringBeginnerEasy/src/test/java/com/example/SpringBeginnerEasy/SpringBeginnerEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBeginnerEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeginnerEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
