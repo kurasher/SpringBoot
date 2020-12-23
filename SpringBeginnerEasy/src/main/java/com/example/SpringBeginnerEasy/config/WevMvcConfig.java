aecb1d5e7674c66bb519e909381641bd75214f01
kurasher <27tonokurasher07@gmail.com>
2020-12-23 21:14:58 +0900

    [add] いろいろ

---
+++ SpringBeginnerEasy/src/main/java/com/example/SpringBeginnerEasy/config/WevMvcConfig.java
@@ -0,0 +1,9 @@
+package com.example.SpringBeginnerEasy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WevMvcConfig implements WebMvcConfigurer {
+
+}
